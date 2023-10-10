685c151df0401d065bea00b14eb091f7cdcc4c7c
Malik <malik@malik-alhudrub-pc.scoiumtech.local>
2023-10-10 16:34:13 +0300

    cleaned: features test, pageObject and  added 4 coomands

--- cypress.config.js
+++ cypress.config.js
@@ -3,10 +3,21 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      overrideConfig = {}
+      if(config.userAgent === 'mobile'){
+        overrideConfig.viewportWidth= 375,
+        overrideConfig.viewportHeight = 667, 
+        overrideConfig.userAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X)AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75Mobile/14E5239e Safari/602.1"
+     
+      }else if(config.userAgent === 'desktop'){
+        overrideConfig.viewportWidth = 1920,
+        overrideConfig.viewportHeight = 1080,
+        overrideConfig.userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
+      }
+      return {...config, ...overrideConfig}
     },
     baseUrl: 'https://www.saucedemo.com',
-    pageLoadTimeout: 120000,
+    pageLoadTimeout: 60000,
 
   },
   chromeWebSecurity: false,
